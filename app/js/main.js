8db9282f747c080224e4de0b30974ba2df602ec0
Meztake <gameholin123@mail.ru>
2021-01-19 17:56:52 +0300

    project

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,42 @@
 $(function(){
-    
+    $('.portfolio__slider').slick({
+        dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  arrows: false,
+  dotsClass: "portfolio-dots",
 });
+
+
+$('.project-slides__slider').slick({
+  dots: true,
+infinite: true,
+speed: 500,
+fade: true,
+cssEase: 'linear',
+arrows: false,
+dotsClass: "project-slides__dots",
+});
+
+
+$('.testimonials__slider').slick({
+  arrows:false,
+  dots:true,
+  infinite: true,
+  speed: 300,
+  centerMode: true,
+  dotsClass: "testimonials__dots",
+  variableWidth: true
+
+});
+$('.arrows').on('click', function(e){
+  $('html,body').stop().animate({ scrollTop: $('#top').offset().top }, 1000);
+  e.preventDefault();
+});
+
+new WOW().init();
+});
+
+
